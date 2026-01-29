31b1e3586b849f028c8f1b65dfb8e23a3daec679
lgt505 <75490206+lgt505@users.noreply.github.com>
2026-01-29 14:37:21 +0000

    Update Correlation_concat.py
    Removed old code

--- ACRUISE-3 Data/Correlation_concat.py
+++ ACRUISE-3 Data/Correlation_concat.py
@@ -13,163 +13,6 @@ import glob
 import matplotlib.pyplot as plt
 import scipy.optimize
 
-#LIF_C284 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE_ACSIS\C284_290422\LIF_time_avg_all.xlsx')
-#FAAM_C284 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE_ACSIS\C284_290422\FAAM_time_avg_all.xlsx')
-
-LIF_C285 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\Final_LIF_SO2_mr_10s_tm_2.xlsx')
-FAAM_C285 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\Final_FAAM_SO2_mr_10s_tm.xlsx')
-CIMS_C285 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\Final_CIMS_SO2_mr_10s_tm.xlsx')
-CIMS_C285_uncorr = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\Final_CIMS_SO2_mr_10s_tm_uncorrected.xlsx')
-CIMS_C285_cps = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\Final_CIMS_cps_10s_tm.xlsx')
-
-LIF_C286 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C286_010522\Data_paper\Final_LIF_SO2_mr_10s_tm_2.xlsx')
-FAAM_C286 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C286_010522\Data_paper\Final_FAAM_SO2_mr_10s_tm.xlsx')
-CIMS_C286 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C286_010522\Data_paper\Final_CIMS_SO2_mr_10s_tm.xlsx')
-CIMS_C286_uncorr = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C286_010522\Data_paper\Final_CIMS_SO2_mr_10s_tm_uncorrected.xlsx')
-CIMS_C286_cps = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C286_010522\Data_paper\Final_CIMS_cps_10s_tm.xlsx')
-
-LIF_C287 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C287_020522\Data_paper\Final_LIF_SO2_mr_10s_tm_2.xlsx')
-FAAM_C287 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C287_020522\Data_paper\Final_FAAM_SO2_mr_10s_tm.xlsx')
-CIMS_C287 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C287_020522\Data_paper\Final_CIMS_SO2_mr_10s_tm.xlsx')
-CIMS_C287_uncorr = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C287_020522\Data_paper\Final_CIMS_SO2_mr_10s_tm_uncorrected.xlsx')
-CIMS_C287_cps = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C287_020522\Data_paper\Final_CIMS_cps_10s_tm.xlsx')
-
-
-LIF_all = [LIF_C285, LIF_C286, LIF_C287]
-
-FAAM_all = [FAAM_C285, FAAM_C286, FAAM_C287]
-
-CIMS_all = [CIMS_C285, CIMS_C286, CIMS_C287]
-
-CIMS_uncorr = [CIMS_C285_uncorr, CIMS_C286_uncorr, CIMS_C287_uncorr]
-
-CIMS_cps = [CIMS_C285_cps, CIMS_C286_cps, CIMS_C287_cps]
-
-LIF_all_concat = pd.concat(LIF_all, axis=0, ignore_index=True)
-FAAM_all_concat = pd.concat(FAAM_all, axis=0, ignore_index=True)
-CIMS_all_concat = pd.concat(CIMS_all, axis=0, ignore_index=True)
-CIMS_uncorr_concat = pd.concat(CIMS_uncorr, axis=0, ignore_index=True)
-CIMS_cps_concat = pd.concat(CIMS_cps, axis=0, ignore_index=True)
-
-LIF_all_concat.index = pd.to_datetime(LIF_all_concat['Unnamed: 0'], format="%Y/%m/%d %H:%M:%S", utc=True)
-FAAM_all_concat.index = pd.to_datetime(FAAM_all_concat['Time_tm_final'], format="%Y/%m/%d %H:%M:%S", utc=True)
-CIMS_all_concat.index = pd.to_datetime(CIMS_all_concat['Unnamed: 0'], format="%Y/%m/%d %H:%M:%S", utc=True)
-CIMS_cps_concat.index = pd.to_datetime(CIMS_cps_concat['Unnamed: 0'], format="%Y/%m/%d %H:%M:%S", utc=True)
-
-
-LIF_all_concat = LIF_all_concat.dropna()
-CIMS_all_concat = CIMS_all_concat.dropna()
-FAAM_all_concat = FAAM_all_concat.dropna()
-CIMS_cps_concat = CIMS_cps_concat.dropna()
-
-count_below_threshold = (FAAM_all_concat['FAAM_tm_final'] < 0.4).sum()
-
-
-plt.plot(CIMS_all_concat['CIMS_tm_final'], color='green')
-plt.plot(FAAM_all_concat['FAAM_tm_final'], color='orange')
-plt.plot(LIF_all_concat['LIF_tm_final'], color='blue')
-plt.axhline(2, color='green', linestyle='dashed')
-plt.axhline(0.4, color='orange', linestyle='dashed')
-plt.axhline(0.06, color='blue', linestyle='dashed')
-
-#LIF_all_concat.to_excel('LIF_all_concat.xlsx')
-#FAAM_all_concat.to_excel('FAAM_all_concat.xlsx')
-#CIMS_all_concat.to_excel('CIMS_all_concat.xlsx')
-#LIF_CIMS_concat.to_excel('LIF_CIMS_concat.xlsx')
-
-
-# for CIMS-LIF plot
-Corr_both = pd.DataFrame([LIF_all_concat['LIF_tm_final'], CIMS_all_concat['CIMS_tm_final']])
-Corr_both = Corr_both.transpose()
-
-for i in Corr_both.index:
-    if Corr_both['CIMS_tm_final'][i] < 2:
-        Corr_both['CIMS_tm_final'][i] = np.nan
-        Corr_both['LIF_tm_final'][i] = np.nan
-        
-for i in Corr_both.index:
-    if Corr_both['LIF_tm_final'][i] < 2:
-        Corr_both['CIMS_tm_final'][i] = np.nan
-        Corr_both['LIF_tm_final'][i] = np.nan
-    
-
-plt.scatter(Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-Corr_both = Corr_both.dropna()
-#Corr_both.to_excel('Correlation_CIMS_LIF_2ppb_updated.xlsx')
-
-def func(x, a, b):
-    return a * x + b
-#scipy.optimize.curve_fit(func, Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-popt, pcov = scipy.optimize.curve_fit(func, Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-residuals = Corr_both['CIMS_tm_final']- func(Corr_both['LIF_tm_final'], *popt)
-ss_res = np.sum(residuals**2)
-ss_tot = np.sum((Corr_both['CIMS_tm_final']-np.mean(Corr_both['CIMS_tm_final']))**2)
-r_squared = 1 - (ss_res / ss_tot)
-
-
-# for CIMSuncorr-LIF plot
-Corr_both = pd.DataFrame([LIF_all_concat['LIF_tm_final'], CIMS_uncorr_concat['CIMS_tm_final']])
-Corr_both = Corr_both.transpose()
-
-plt.scatter(Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-
-for i in Corr_both.index:
-    if Corr_both['CIMS_tm_final'][i] < 2:
-        Corr_both['CIMS_tm_final'][i] = np.nan
-        Corr_both['LIF_tm_final'][i] = np.nan
-        
-for i in Corr_both.index:
-    if Corr_both['LIF_tm_final'][i] < 2:
-        Corr_both['CIMS_tm_final'][i] = np.nan
-        Corr_both['LIF_tm_final'][i] = np.nan
-    
-
-plt.scatter(Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-Corr_both = Corr_both.dropna()
-#Corr_both.to_excel('Correlation_CIMS_uncorr_LIF_2ppb_updated.xlsx')
-
-def func(x, a, b):
-    return a * x + b
-#scipy.optimize.curve_fit(func, Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-popt, pcov = scipy.optimize.curve_fit(func, Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-residuals = Corr_both['CIMS_tm_final']- func(Corr_both['LIF_tm_final'], *popt)
-ss_res = np.sum(residuals**2)
-ss_tot = np.sum((Corr_both['CIMS_tm_final']-np.mean(Corr_both['CIMS_tm_final']))**2)
-r_squared = 1 - (ss_res / ss_tot)
-
-
-
-# for FAAM-LIF plot
-Corr_both = pd.DataFrame([LIF_all_concat['LIF_tm_final'], FAAM_all_concat['FAAM_tm_final']])
-Corr_both = Corr_both.transpose()
-
-plt.scatter(Corr_both['LIF_tm_final'], Corr_both['FAAM_tm_final'])
-
-for i in Corr_both.index:
-    if Corr_both['FAAM_tm_final'][i] < 0.4:
-        Corr_both['FAAM_tm_final'][i] = np.nan
-        Corr_both['LIF_tm_final'][i] = np.nan
-        
-for i in Corr_both.index:
-    if Corr_both['LIF_tm_final'][i] < 0.4:
-        Corr_both['FAAM_tm_final'][i] = np.nan
-        Corr_both['LIF_tm_final'][i] = np.nan
-
-plt.scatter(Corr_both['LIF_tm_final'], Corr_both['FAAM_tm_final'])
-Corr_both = Corr_both.dropna()
-#Corr_both.to_excel('Correlation_FAAM_LIF_400ppt_updated.xlsx')
-
-def func(x, a, b):
-    return a * x + b
-#scipy.optimize.curve_fit(func, Corr_both['LIF_tm_final'], Corr_both['CIMS_tm_final'])
-popt, pcov = scipy.optimize.curve_fit(func, Corr_both['LIF_tm_final'], Corr_both['FAAM_tm_final'])
-residuals = Corr_both['FAAM_tm_final']- func(Corr_both['LIF_tm_final'], *popt)
-ss_res = np.sum(residuals**2)
-ss_tot = np.sum((Corr_both['FAAM_tm_final']-np.mean(Corr_both['FAAM_tm_final']))**2)
-r_squared = 1 - (ss_res / ss_tot)
-
-
-### rewritten
 
 LIF_FAAM_C285 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\New\Final_LIF_FAAM_SO2_mr_10s_tm.xlsx')
 FAAM_LIF_C285 = pd.read_excel(r'C:\Users\lgt505\Google Drive\Flight Campaigns\ACRUISE\C285_300422\Data_paper\New\Final_FAAM_LIF_SO2_mr_10s_tm.xlsx')
@@ -199,7 +42,6 @@ FAAM_LIF_all_concat = pd.concat(FAAM_LIF_all, axis=0, ignore_index=True)
 LIF_CIMS_all_concat = pd.concat(LIF_CIMS_all, axis=0, ignore_index=True)
 CIMS_LIF_all_concat = pd.concat(CIMS_LIF_all, axis=0, ignore_index=True)
 
-
 LIF_FAAM_all_concat.index = pd.to_datetime(LIF_FAAM_all_concat['Time_tm_final'], format="%Y/%m/%d %H:%M:%S", utc=True)
 FAAM_LIF_all_concat.index = pd.to_datetime(FAAM_LIF_all_concat['Unnamed: 0'], format="%Y/%m/%d %H:%M:%S", utc=True)
 LIF_CIMS_all_concat.index = pd.to_datetime(LIF_CIMS_all_concat['Time_tm_final'], format="%Y/%m/%d %H:%M:%S", utc=True)
@@ -211,7 +53,6 @@ LIF_CIMS_all_concat = LIF_CIMS_all_concat.dropna()
 CIMS_LIF_all_concat = CIMS_LIF_all_concat.dropna()
 
 count_below_threshold = (FAAM_all_concat['FAAM_tm_final'] < 0.4).sum()
-
 
 plt.plot(LIF_FAAM_all_concat['LIF_tm_final'], color='blue')
 plt.plot(FAAM_LIF_all_concat['FAAM_tm_final'], color='orange')
